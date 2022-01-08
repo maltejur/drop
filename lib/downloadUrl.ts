5cfaa3dfdf0284e0f73733a0c22ebc23d32a51a5
Malte Jürgens <maltejur@dismail.de>
2022-01-08 19:33:10 +0100

    wording

--- lib/downloadUrl.ts
+++ lib/downloadUrl.ts
@@ -5,7 +5,7 @@ if (!DOWNLOAD_URL)
   if (NODE_ENV === "development") DOWNLOAD_URL = "http://localhost:3000";
   else
     throw new Error(
-      "DOWNLOAD_URL is not defined, it has to point to the URL where the files are hosted (for example with nginx)"
+      "NEXT_PUBLIC_DOWNLOAD_URL is not defined, it has to point to the URL where the files are hosted (for example with nginx)"
     );
 
 export default DOWNLOAD_URL;
