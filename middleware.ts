f74b3670715f16c5b1130cf3b26a4f3d7e5c7216
Malte Jürgens <maltejur@dismail.de>
2025-01-11 20:40:03 +0100

    The fix update :DD

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 export const config = {
-  matcher: "/api/:function*",
+  matcher: ["/api/drop/:function*", "/api/upload/:function*", "/api/auth"],
 };
 
 import { isValidPass } from "lib/auth";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (!isValidPass(req.cookies.get("pass").value)) {
+  if (!isValidPass(req.cookies.get("pass")?.value)) {
     return new Response("Unauthorized", { status: 401 });
   }
 }
